f22c408682eb8004d747ab64154d07c1aafd985e
AlphaTangoFoxtrot <randyryee@gmail.com>
2026-02-27 01:36:08 -0500

    Added cron job for fetching events from TimeOut

---
+++ app/_cron/extractTimeoutEvents.mjs
@@ -0,0 +1,59 @@
+import * as cheerio from "cheerio"
+
+async function getEvents() {
+  const domain = "www.timeout.com"
+  const url = `https://${domain}/newyork/things-to-do/things-to-do-in-new-york-this-week`
+
+  console.info("Fetching events...")
+  const res = await fetch(url, {
+    headers: {
+    "User-Agent": "Mozilla/5.0",
+    },
+    cache: "no-store"
+  })
+
+  if(!res.ok) {
+    return [{error: `HTTP ${res.status}`}, {status: 500}]
+  }
+
+  const html = await res.text();
+  const $ = cheerio.load(html)
+
+  console.info("Parsing...")
+  // Get list with only the events listed.
+  const eventList = $("body").find("[data-zone-name='large_list']").first()
+
+  const headers = $(eventList).find("div >* h3")
+  const summaries = $(eventList).find("div >* [data-testid='summary_testID']")
+  const links = $(eventList).find("div >* a:has(h3)")
+
+  const eventTitles = headers.map((_, el) => {
+    // Remove all non alphanumeric characters except for spaces
+    return $(el).text()
+  }).get()
+
+  const eventSummaries = summaries.map((_, el) => {
+    return $(el).text()
+  }).get()
+
+  const eventLinks = links.map((_, el) => {
+    return `https://${domain}/${$(el).attr("href")}`
+  }).get()
+
+  const response = {
+    from: "TimeOut",
+    events: eventTitles.map((title, i) => ({
+      title,
+      summary: eventSummaries[i] ?? "", // Fallback if list is shorter
+      link: eventLinks[i] ?? ""
+    })
+    )
+  }
+  console.log(JSON.stringify(response))
+  return {content: JSON.stringify(response)}
+}
+
+// Execute only if file is called directly and not imported
+if (require.main === "module") {
+  getEvents()
+}
